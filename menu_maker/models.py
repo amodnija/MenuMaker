c0a378ed1fcabe468f2507c0cc0f8cf8e82947af
Amod Nijasure <amodnij123@gmail.com>
2020-09-26 21:47:22 +0530

    Making Models

--- menu_maker/models.py
+++ menu_maker/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Menu(models.Model):
+    product = models.CharField("Product")
+    weight = models.CharField("Weight")
+    cut_details = models.CharFiels("Cut Details")
+    count = models.CharField("Count Per Kg")
+    rate = models.CharField("Rate")
+    availability = models.CharField(
+        "Availibility", 
+        choices=[("yes", "Yes"),
+                 ("no", "No")],
+                                    
+        )
